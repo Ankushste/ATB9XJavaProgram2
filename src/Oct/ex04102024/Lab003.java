68464817214abc9361b6e1f19b531ec444a9644a
HP <ankushchohan384@gmail.com>
2024-10-08 20:12:30 +0530

    First file

--- src/Oct/ex04102024/Lab003.java
+++ src/Oct/ex04102024/Lab003.java
@@ -1,4 +1,12 @@
 package Oct.ex04102024;
 
 public class Lab003 {
+    public static void main(String[] args){
+        System.out.println("Multiply");
+        System.out.println(5*2);
+        System.out.println(2*8);
+        System.out.println(8*6);
+        System.out.println(7*7);
+        System.out.println(9*9);
+    }
 }
